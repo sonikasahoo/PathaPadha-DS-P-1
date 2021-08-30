a873ee53bba06caa8546577254a25ac77af6cb4b
sonikasahoo <64542870+sonikasahoo@users.noreply.github.com>
2021-08-30 21:06:20 +0530

    Delete swapping_using 3rd variable.py

--- swapping_using 3rd variable.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("Enter the 1st number : "))
-num2=int(input("Enter the 2nd number : "))
-print("\nBefore swapping \n1st number : ",num1,"\n2nd number : ",num2)
-print("\n")
-temp=num1
-num1=num2
-num2=temp
-print("After swapping \n1st number : ",num1,"\n2nd number : ",num2)
