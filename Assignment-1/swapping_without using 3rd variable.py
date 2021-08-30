861faa4857e27c6f4dd748e8cd9164ba5762738f
sonikasahoo <64542870+sonikasahoo@users.noreply.github.com>
2021-08-30 21:05:23 +0530

    Add files via upload

---
+++ Assignment-1/swapping_without using 3rd variable.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the 1st number : "))
+num2=int(input("Enter the 2nd number : "))
+print("\nBefore swapping \n1st number : ",num1,"\n2nd number : ",num2)
+print("\n")
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After swapping \n1st number : ",num1,"\n2nd number : ",num2)
