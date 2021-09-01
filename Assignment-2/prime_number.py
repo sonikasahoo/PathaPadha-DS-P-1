9841da7b2d35798b24a2717c719123dac9d8762c
Sonika Sahoo <64542870+sonikasahoo@users.noreply.github.com>
2021-09-01 09:37:28 +0530

    Update prime_number.py

--- Assignment-2/prime_number.py
+++ Assignment-2/prime_number.py
@@ -6,7 +6,7 @@ for i in range(2,8):
         print(num,"number is not a prime number")
         break
     else:
-        count=+1
+        count+=1
 while(count==6):
     print(num,"number is a prime number")
     break
