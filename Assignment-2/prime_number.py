c0be63328806bf7491831c28b49540de006179d5
Sonika Sahoo <64542870+sonikasahoo@users.noreply.github.com>
2021-09-01 09:32:36 +0530

    Update prime_number.py

--- Assignment-2/prime_number.py
+++ Assignment-2/prime_number.py
@@ -1,9 +1,12 @@
 #Prime number or, not
 num=int(input("Enter the number : "))
+count=0
 for i in range(2,8):
     if(num%i==0):
         print(num,"number is not a prime number")
         break
     else:
-        print(num,"number is a prime number")
-        break
+        count=+1
+while(count==6):
+    print(num,"number is a prime number")
+    break
