90958372ee3911bc68414fa63acd9c5e630b3732
Sonika Sahoo <64542870+sonikasahoo@users.noreply.github.com>
2021-09-17 01:29:46 +0530

    Add files via upload

---
+++ Assignment-7/Sorting_a dictionary using lambda function.py
@@ -0,0 +1,4 @@
+#Write a Python program to sort a list of dictionaries using Lambda.
+models = [{'make':'Nokia', 'model':216, 'color':'Black'}, {'make':'Mi Max', 'model':2, 'color':'Gold'}, {'make':'Samsung', 'model': 7, 'color':'Blue'}]
+r=sorted(models,key=lambda x:x["model"])
+print(list(r))
